28ebc0616af63326cc0f0ddd9581e9f183b30a05
Sakchai47511 <77142182+Sakchai47511@users.noreply.github.com>
2021-06-02 23:14:59 +0700

    Add files via upload

---
+++ โปรเจคC#_Minesweeper/Minesweeper/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProjektProgram
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnection databaseConnection()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=stock;charset=utf8;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            return conn;
+        }
+
+        //ทำปุ่มระดับความยาก
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int diff = 1;
+          
+            Program.Form.startGame(diff);
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int diff = 2;
+          
+            Program.Form.startGame(diff);
+            Close();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int diff = 3;
+
+          
+            Program.Form.startGame(diff);
+            Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ โปรเจคC#_Minesweeper/Minesweeper/Form4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProjektProgram
+{
+    public partial class Form4 : Form
+    {
+        private MySqlConnection databaseConnection()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=stock;charset=utf8;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            return conn;
+        }
+
+        public string stat;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string pltime = Convert.ToString(Program.pltime);
+            MessageBox.Show("Saved!!");
+            MySqlConnection con = databaseConnection();
+            MySqlCommand cmd = con.CreateCommand();
+            con.Open();
+            cmd.CommandText = $"INSERT INTO som(Name,Time) VALUES (\"{textBox1.Text}\",@time)";
+            cmd.Parameters.AddWithValue("@time", pltime);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            this.Hide();
+            stat = "1";
+        }
+    }
+}
