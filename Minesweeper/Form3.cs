fed2cb3511f159b71f2bc634a41e6563b96ae73d
Sakchai47511 <77142182+Sakchai47511@users.noreply.github.com>
2021-06-16 00:48:12 +0700

    Pro C#

---
+++ Minesweeper/Form3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProjectProgram
+
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnection databaseConnection()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=stock;charset=utf8;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            return conn;
+        }
+
+        private void showEquipment()
+        {
+            MySqlConnection conn = databaseConnection();
+            DataSet ds = new DataSet();
+            conn.Open();
+            MySqlCommand cmd;
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM score01";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            adapter.Fill(ds);
+            conn.Close();
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+        }
+
+
+
+
+
+        //ไม่ได้ใช้//
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            showEquipment();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Minesweeper/Form4.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProjectProgram
+{
+    public partial class Form4 : Form
+    {
+        string[] AQ = { "Time_EZ", "Time_Med", "Time_Hard" };
+
+        private MySqlConnection databaseConnection()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=stock;charset=utf8;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            return conn;
+        }
+
+        public string stat;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = databaseConnection();
+            conn.Open();
+            MySqlCommand cmd;
+            cmd = conn.CreateCommand();
+            cmd.CommandText = $"SELECT * FROM score01 WHERE Name=\"{textBox1.Text}\"";
+            MySqlDataReader row = cmd.ExecuteReader();
+            if (row.HasRows)
+            {
+                while (row.Read())
+                {
+                    if (int.Parse(row.GetString(Program.check)) > Program.time_form||row.GetString(Program.check)=="")
+                    {
+                        conn = databaseConnection();
+                        conn.Open();
+                        string sql = "UPDATE score01 SET " + AQ[Program.check - 1] + "='" + Program.time_form + "'WHERE Name='" + textBox1.Text + "'";
+                        cmd = new MySqlCommand(sql, conn);
+                        cmd.ExecuteReader();
+                        conn.Close();
+                        MessageBox.Show("Saved!!");
+                        Program.quantity = 0;
+                        this.Hide();
+
+                    }
+                }
+            }
+
+            else
+            {
+                if (Program.check == 1)
+                {
+                    MySqlConnection con = databaseConnection();
+                    con.Open();
+                    string sql1 = "INSERT INTO score01(Name,Time_EZ,Time_Med,Time_Hard) VALUES('" + textBox1.Text + "','" + Program.time_form + "','','')";
+                    MySqlCommand cmd1 = new MySqlCommand(sql1, con);
+                    cmd1.ExecuteReader();
+                    con.Close();
+                    MessageBox.Show("Saved!!");
+                    Program.quantity = 0;
+                    this.Hide();
+                }
+                else if (Program.check == 2)
+                {
+                    MySqlConnection con = databaseConnection();
+                    con.Open();
+                    string sql1 = "INSERT INTO score01(Name,Time_EZ,Time_Med,Time_Hard) VALUES('" + textBox1.Text + "','','" + Program.time_form + "','')";
+                    MySqlCommand cmd1 = new MySqlCommand(sql1, con);
+                    cmd1.ExecuteReader();
+                    con.Close();
+                    MessageBox.Show("Saved!!");
+                    Program.quantity = 0;
+                    this.Hide();
+                }
+                else
+                {
+                    MySqlConnection con = databaseConnection();
+                    con.Open();
+                    string sql1 = "INSERT INTO score01(Name,Time_EZ,Time_Med,Time_Hard) VALUES('" + textBox1.Text + "','','','" + Program.time_form + "')";
+                    MySqlCommand cmd1 = new MySqlCommand(sql1, con);
+                    cmd1.ExecuteReader();
+                    con.Close();
+                    MessageBox.Show("Saved!!");
+                    Program.quantity = 0;
+                    this.Hide();
+                }
+            }
+        }
+
+        //string pltime = Convert.ToString(Program.pltime);
+        //MessageBox.Show("Saved!!");
+        //MySqlConnection con = databaseConnection();
+        //MySqlCommand cmd = con.CreateCommand();
+        //con.Open();
+        //cmd.CommandText = $"INSERT INTO score01(Name,Time) VALUES (\"{textBox1.Text}\",@time)";
+        //cmd.Parameters.AddWithValue("@time", pltime);
+        //cmd.ExecuteNonQuery();
+        //con.Close();
+        //this.Hide();
+
+
+
+
+    }
+}
